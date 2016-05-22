567d8bcc8ef7f54766d5f56a70de03846d42c9e0
JJ Wong <jjwong0915@gmail.com>
2016-05-22 22:10:53 +0800

    rebuild structure

--- compiler/parser.cpp
+++ compiler/parser.cpp
@@ -23,6 +23,7 @@ using ascii::char_;
 using ascii::space;
 using ascii::space_type;
 
+
 // grammer
 struct syntax : grammar<string::iterator, vector<string>()> {
 	syntax(): syntax::base_type(cmd) {
@@ -32,65 +33,103 @@ struct syntax : grammar<string::iterator, vector<string>()> {
 };
 
 
-// ast structs
+// AST structs
 struct syntax_tree;
 
 struct syntax_tree {
  	int indent;
- 	vector<string> content;
+ 	vector<string> tokens;
  	vector<syntax_tree> children;
 };
 
+typedef vector<syntax_tree>::iterator syntax_tree_iterator;
 
-// debug
-string tab(int n) {
-	string s;
-	for(int i=0;i<n;i++) {
-		s += " ";
+
+// process syntax
+bool for_syntax(vector<syntax_tree_iterator>& now) {
+	vector<string>& ts = now.back()->tokens;
+	syntax_tree_iterator parent = *(--now.end());
+	if(ts.front() == "if") {
+		ts.insert(++ts.begin(), "(");
+		ts.insert(ts.end(), ") {");
+		syntax_tree temp;
+		temp.indent = parent->indent;
+		temp.tokens.push_back("}");
+		parent->children.insert(++now.back(), temp);
+		return true;
+	} else {
+		return false;
 	}
-	return s;
 }
-void print(syntax_tree t) {
-	cout << tab(t.indent);
-	for(string i : t.content) {
-		cout << i << " ";
+
+vector<bool (*)(vector<syntax_tree_iterator>&)> syntaxes = {for_syntax};
+
+void process_syntax(vector<syntax_tree_iterator>& now) {
+	vector<syntax_tree>& chd = now.back()->children;
+	for(auto i=chd.begin();i!=chd.end();i++) {
+		now.push_back(i);
+		process_syntax(now);
+		now.pop_back();
 	}
-	cout << "{" << endl;
-	for(syntax_tree i : t.children) {
-		print(i);
+	for(auto& i : syntaxes) {
+		bool end = i(now);
+		if(end) {
+			break;
+		}
 	}
-	cout << tab(t.indent) << "}" << endl;
 }
 
 
-// modify ast
-void insert_node(vector<syntax_tree*>& now, vector<string> input) {
+// modify AST 
+void insert_node(vector<syntax_tree_iterator>& now, vector<string> data) {
 	syntax_tree t;
-	t.indent = input[0].length();
-	t.content.insert(t.content.end(), ++input.begin(), input.end());
+	t.indent = data[0].length();
+	t.tokens.insert(t.tokens.end(), ++data.begin(), data.end());
 	now.back()->children.push_back(t);
-	now.push_back(&(now.back()->children.back()));
+	now.push_back(--now.back()->children.end());
 }
-void modify(vector<syntax_tree*>& now, vector<string> input) {
-	int input_indent = input[0].length();
-	int now_indent = now.back()->indent;
 
+int check_indent(vector<syntax_tree_iterator>& now, int input_indent) {
+	int now_indent = now.back()->indent;
+	//cout << "now indent: " << now_indent << endl;
 	if(input_indent == now_indent) {
 		now.pop_back();
-		insert_node(now, input);
-	} else if(input_indent > now_indent) {
-		insert_node(now, input);
 	} else if(input_indent < now_indent) {
 		while(now.back()->indent > input_indent) {
 			now.pop_back();
 		}
-		if(now.back()->indent == input_indent) {
-			now.pop_back();
-			modify(now, input);
-		} else {
+		if(now.back()->indent < input_indent) {
 			cout << "[error] indent error" << endl;
+			return 1;
 		}
 	}
+	return 0;
+}
+
+
+// output
+void print(syntax_tree t) {
+	if(t.indent >= 0) {
+		for(int i=0;i<t.indent;i++) {
+			cout << " ";
+		}
+		for(string s : t.tokens) {
+			cout << s << " ";
+		}
+		cout << endl;
+	}
+	for(syntax_tree i : t.children) {
+		print(i);
+	}
+}
+
+
+//debug
+void print_now(vector<syntax_tree_iterator>& now) {
+	for(syntax_tree_iterator i : now) {
+		cout << i->tokens[0] << " ";
+	}
+	cout << endl;
 }
 
 
@@ -99,27 +138,38 @@ int main() {
 
 	string s;
 	syntax expr;
-	syntax_tree ast;
-	vector<syntax_tree*> now;
+	syntax_tree root, temp;
+	vector<syntax_tree_iterator> now;
 
-	ast.indent = -1;
-	ast.content.push_back("module");
-	now.push_back(&ast);
+	temp.indent = -1;
+	temp.tokens.push_back("module");
+	root.children.push_back(temp);
+	now.push_back(--root.children.end());
 
 	while(getline(cin, s)) {
 		string::iterator begin = s.begin();
-		vector<string> input;
-		bool ok = parse(begin, s.end(), expr, input);
+		vector<string> data;
+		bool ok = parse(begin, s.end(), expr, data);
 		if(ok && begin == s.end()) {
-			modify(now, input);
-		} else {
-			cout << "Failed" << endl;
+			int status = check_indent(now, data[0].length());
+			//cout << status << endl;
+			if(!status) {
+				insert_node(now, data);
+			} else {
+				return 1;
+			}
+			//print(root.children.back());
 		}
 	}
 
-	for(syntax_tree i : ast.children) {
-		print(i);
+	while(now.size() > 1) {
+		now.pop_back();
 	}
+	process_syntax(now);
+
+	print(root.children.back());
+
+	return 0;
 }
 
 
